9b36eac12aefb555bda5fd95d4d2382fc784863b
beluy452 <beluy452@yandex.ru>
2016-09-02 11:32:12 +0300

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    $(function (){
+        var images = [
+                './img/slide/slide-1.jpg',
+                './img/slide/slide-2.jpg',
+                './img/slide/slide-3.jpg'
+            ],
+            header = $('header'),
+            imageIndex = 0;
+
+        $('.navigation-but').click(function() {
+
+            if ($(this).attr('rel') == "next") {
+                console.log('next');
+                if (imageIndex  < images.length - 1) {
+                    header.css('background-image', 'url('+images[++imageIndex ]+')')
+                }else{
+                    imageIndex = 0;
+                    header.css('background-image', 'url('+images[imageIndex]+')');
+                }
+            }
+            if($(this).attr('rel') == "prev") {
+                console.log('prev');
+                if (imageIndex  > 0) {
+                    header.css('background-image', 'url('+images[--imageIndex ]+')')
+                }else{
+                    imageIndex  = images.length - 1 ;
+                    header.css('background-image', 'url('+images[imageIndex]+')');
+                }
+            }
+            return false;
+        });
+    });
+
+    $('.nav li a[href^="#"]').on('click', function(event){
+        event.preventDefault();
+        var anchor = this.hash;
+        $('html, body').stop().animate({
+            scrollTop: $(anchor).offset().top - 10
+        }, 1500);
+    });//scroll якорів
+
+});
